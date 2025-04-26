15b44422b5fbc07a275975ffde4f191d3f0457a2
Antonio Pérez <apema2079@gmail.com>
2025-04-25 18:47:35 -0600

    fat: initial commit with base configs

---
+++ lambda.tf
@@ -0,0 +1,37 @@
+
+
+locals {
+   lambda_code_relative_path = "${path.module}#The relative or absolute path to the lambda code"
+  files_to_include = [
+    # the files to include for debugging this lambda
+    # The list of siles have to include only the implementation code
+    # not the setup.py, and so on
+  ]
+}
+
+data "archive_file" "lambda_code" {
+  type             = "zip"
+  source_dir       = local.lambda_code_relative_path
+  output_path      = "${path.module}/lambda_function_payload.zip"
+  output_file_mode = "0666"
+
+  excludes = [for file in fileset(local.lambda_code_relative_path, "**/*") : file if !contains(local.files_to_include, file)]
+}
+
+resource "aws_lambda_function" "lambda_function" {
+  filename         = data.archive_file.lambda_code.output_path
+  function_name    = "crx_ws_notification"
+  role             = "arn:aws:iam::000000000000:role/lambda-role"
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.12"
+  source_code_hash = data.archive_file.lambda_code.output_base64sha256
+
+  environment {
+    variables = {
+      ENVIRONMENT         = "dev"
+      SECRET_ENV_VAR_NAME = aws_secretsmanager_secret.oracle_secret.name
+    }
+  }
+
+  depends_on = [docker_container.localstack]
+}
---
+++ providers.tf
@@ -0,0 +1,21 @@
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+provider "aws" {
+  region                      = "us-west-2"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  skip_region_validation      = true
+  s3_use_path_style           = true
+
+  endpoints {
+    iam            = "http://localhost:4566"
+    lambda         = "http://localhost:4566"
+    secretsmanager = "http://localhost:4566"
+    sqs            = "http://localhost:4566"
+  }
+}
