d9776ce6cd04dc4b1f7f0efe44ac4001c9212a00
Antonio Pérez <apema2079@gmail.com>
2025-05-12 07:24:29 -0600

    feat: update files for custom example

--- ddb-lambda-sqs-example/localstack.tf
+++ ddb-lambda-sqs-example/localstack.tf
@@ -15,7 +15,7 @@ resource "docker_container" "localstack_container" {
   image = "localstack/localstack:stable"
 
   env = [
-    "SERVICES=iam,lambda,ddb,sqs,",
+    "SERVICES=iam,lambda,dynamodb,sqs,",
     "DOCKER_HOST=unix:///var/run/docker.sock",
     "AWS_DEFAULT_REGION=us-west-2",
     "GATEWAY_LISTEN=4566",
@@ -25,11 +25,10 @@ resource "docker_container" "localstack_container" {
     "LAMBDA_DOCKER_FLAGS=-v /var/run/docker.sock:/var/run/docker.sock -p 19891:19891",
     "ENVIRONMENT=dev",
     "DEBUG=1",
-    "ENVIRONMENT=dev",
-    "SECRET_ENV_VAR_NAME=localstack_oracle_dev_credentials"
+    "ENVIRONMENT=testing",
   ]
 
-  network_mode = docker_network.localstack-network.name
+  network_mode = docker_network.localstack_network.name
 
   remove_volumes = true
 
@@ -56,15 +55,15 @@ resource "docker_container" "localstack_container" {
   # Localstack persistence volume mount
   volumes {
     container_path = "/var/lib/localstack"
-    volume_name    = docker_volume.localstack-volume.name
+    volume_name    = docker_volume.localstack_volume.name
   }
 
   depends_on = [
-    docker_network.localstack-network,
-    docker_volume.localstack-volume
+    docker_network.localstack_network,
+    docker_volume.localstack_volume
   ]
 }
 
 output "localstack" {
-  value = docker_container.localstack.name
+  value = docker_container.localstack_container.name
 }
--- ddb-lambda-sqs-example/providers.tf
+++ ddb-lambda-sqs-example/providers.tf
@@ -13,9 +13,9 @@ provider "aws" {
   s3_use_path_style           = true
 
   endpoints {
-    iam            = "http://localhost:4566"
-    lambda         = "http://localhost:4566"
-    ddb            = "http://localhost:4566"
-    sqs            = "http://localhost:4566"
+    iam      = "http://localhost:4566"
+    lambda   = "http://localhost:4566"
+    dynamodb = "http://localhost:4566"
+    sqs      = "http://localhost:4566"
   }
 }
